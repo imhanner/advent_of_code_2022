b1e0fca746eba539b99c0d17a680ccf21958cf3e
HT <h.thalheim@shc-solutions.com>
2022-12-05 17:43:26 +0100

    Refactor common code of day 04 into its own file

--- 04/part1.main.kts
+++ 04/part1.main.kts
@@ -1,25 +1,11 @@
 #!/usr/bin/kotlinc -script
 
-import java.io.File
+@file:Import("common.kt")
 
-
-fun String.toIntRange(): IntRange =
-    this.split("-")
-        .let { (start, end) -> start.toInt() .. end.toInt() }
 
 operator fun IntRange.contains(other: IntRange): Boolean =
     (this.first <= other.first && other.last <= this.last) || (other.first <= this.first && this.last <= other.last)
 
-File("./input.txt")
-    .readLines()
-    .map { line ->
-        val range = line
-            .split(",")
-            .let { it[0].toIntRange() to it[1].toIntRange() }
-
-        if (range.first in range.second)
-            return@map 1
-
-        return@map 0
-    }
-    .sum() // 584
+count { range ->
+    range.first in range.second
+} // 584
--- 04/part2.main.kts
+++ 04/part2.main.kts
@@ -1,26 +1,12 @@
 #!/usr/bin/kotlinc -script
 
-import java.io.File
+@file:Import("common.kt")
 
-
-fun String.toIntRange(): IntRange =
-    this.split("-")
-        .let { (start, end) -> start.toInt() .. end.toInt() }
 
 infix fun IntRange.overlap(other: IntRange): Boolean =
     this.intersect(other)
         .isNotEmpty()
 
-File("./input.txt")
-    .readLines()
-    .map { line ->
-        val range = line
-            .split(",")
-            .let { it[0].toIntRange() to it[1].toIntRange() }
-
-        if (range.first overlap range.second)
-            return@map 1
-
-        return@map 0
-    }
-    .sum() // 933
+count { range ->
+    range.first overlap range.second
+} // 933
